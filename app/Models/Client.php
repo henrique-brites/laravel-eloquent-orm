1df30ca20adab34c808a6618aea0bfe5f76e18b8
Henrique Brites <henriquebrites@live.com>
2021-10-24 21:37:32 -0400

    feature: customer model for first relationship

---
+++ app/Models/Client.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    const TYPE_INDIVIDUAL = 'individual';
+    const TYPE_LEGAL = 'legal';
+
+    const MARITAL_STATUS = [
+        1 => 'Solteiro',
+        2 => 'Casado',
+        3 => 'Divorciado'
+    ];
+
+    protected $fillable = [
+        'name',
+        'document_number',
+        'email',
+        'phone',
+        'defaulter',
+        'date_birth',
+        'sex',
+        'marital_status',
+        'physical_disability',
+        'company_name',
+        'client_type',
+        //'soccer_team_id'
+    ];
+}
