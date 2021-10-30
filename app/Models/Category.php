633003483aca10036e6239cf8826bd4b92708485
Henrique Brites <henriquebrites@live.com>
2021-10-30 14:17:24 -0400

    feature: query builder - custom query builder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
@@ -17,3 +19,15 @@ class Category extends Model
 //tabela sing. _ tabela sing.
 //
 // category_product
+
+/** @var Builder $category */
+/*$category = new Category();
+$query = $category->where()
+                ->where()
+                ->where()
+$query->where()
+    $results = $query->get();*/
+
+/** @var Collection $results */
+/*$results = Category::all();
+$results->pluck()*/
